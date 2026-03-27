e988d1e150dc9441b01e16a8bf24ab05e9d423a4
Robin <robin.pierson@grenoble-inp.org>
2026-03-27 17:09:21 +0100

    fsm

--- vhd/fsm.vhd
+++ vhd/fsm.vhd
@@ -18,85 +18,79 @@ end fsm;
 
 architecture behav of fsm is
 
-  type state_type is (IDLE, GET_SAMPLE, MAC_COMPUTE, UPDATE_OUTPUT);
+  type state_type is (INIT, DATA_WAIT, DELAY_LINE_SHIFT, DATA_REQUEST, MULT, DATA_OUT);
   signal curr_state, next_state : state_type;
-  signal count, next_count : unsigned(4 downto 0);
+  signal count, next_count : unsigned(5 downto 0);
 
 begin
 
+  -- Processus synchrone : Mise à jour de l'état et du compteur à chaque front d'horloge
   process(clk, reset)
   begin
     if reset = '1' then
-      curr_state <= IDLE;
-      count      <= (others => '0');
+      curr_state <= INIT;
+      count <= (others => '0');
     elsif rising_edge(clk) then
       curr_state <= next_state;
-      count      <= next_count;
+      count <= next_count;
     end if;
   end process;
 
+  -- Processus combinatoire : Logique du prochain état et des sorties
   process(curr_state, count, adc_data_ready)
   begin 
     next_state <= curr_state;
     next_count <= count;
-
-    case curr_state is
-      
-      when IDLE =>
-        if adc_data_ready = '1' then
-          next_state <= GET_SAMPLE;
-        end if;
-
-      when GET_SAMPLE =>
-        next_count <= (others => '0');
-        next_state <= MAC_COMPUTE;
-
-      when MAC_COMPUTE =>
-        if count = 31 then
-          next_state <= UPDATE_OUTPUT;
-        else
-          next_count <= count + 1;   
-        end if;
-
-      when UPDATE_OUTPUT =>
-        next_state <= IDLE;         
-
-    end case;
-  end process;
-
-  process(curr_state, count)
-  begin 
+ 
     adc_data_request        <= '0';
-    dac_conv_data           <= '0';
+    accu_ctrl               <= '0';
     delay_line_sample_shift <= '0';
-    accu_ctrl               <= '1';
     buff_oe                 <= '0';
+    dac_conv_data           <= '0';
     rom_address             <= (others => '0');
     delay_line_address      <= (others => '0');
 
     case curr_state is
-      
-      when IDLE => 
-        null;
+      when INIT =>
+        next_count <= (others => '0');
+        next_state <= DATA_WAIT;
 
-      when GET_SAMPLE => 
-        adc_data_request        <= '1';
-        delay_line_sample_shift <= '1';
-        accu_ctrl               <= '0'; 
-        
-      when MAC_COMPUTE =>
-        rom_address        <= std_logic_vector(count); 
-        delay_line_address <= std_logic_vector(not count);
-        
-        if count = 0 then
-          accu_ctrl <= '0'; 
-        else
-          accu_ctrl <= '1'; 
+      when DATA_WAIT =>
+        if adc_data_ready = '1' then
+          next_state <= DELAY_LINE_SHIFT;
         end if;
 
-      when UPDATE_OUTPUT =>
+      when DELAY_LINE_SHIFT =>
+        delay_line_sample_shift <= '1';
+        next_state <= DATA_REQUEST;
+
+      when DATA_REQUEST =>
+        adc_data_request <= '1';
+        next_count <= count + 1; 
+    
+        if count = 31 then
+          next_state <= MULT;
+        else
+          next_state <= DATA_WAIT;
+        end if;
+
+      when MULT =>
+        accu_ctrl <= '1';
+        next_count <= count - 1;    
+        rom_address        <= std_logic_vector(to_unsigned(32 - to_integer(count), 5));
+        delay_line_address <= std_logic_vector(to_unsigned(to_integer(count) - 1, 5));
+ 
+        if count = 1 then
+          next_state <= DATA_OUT;
+        end if;
+
+      when DATA_OUT =>
         buff_oe       <= '1';
         dac_conv_data <= '1';
+        next_state    <= INIT;
+
+      when others =>
+        next_state <= INIT;
 
     end case;
   end process;
