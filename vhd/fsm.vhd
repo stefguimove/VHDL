eee064d10dd5b66182e9d5b3131df39568b0c696
Hugo Martinez <hugo.martinez@grenoble-inp.org>
2026-04-03 16:55:57 +0200

    STEEEEUplé2

--- vhd/fsm.vhd
+++ vhd/fsm.vhd
@@ -33,6 +33,7 @@ begin
             current_state <= INIT; 
         elsif rising_edge(clk) then
             current_state <= next_state;
+            count         <= next_count;
         end if;
     end process;
 
